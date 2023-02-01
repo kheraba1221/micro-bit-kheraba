6fb4fce5b53548f34d54369adf9df5acea0969a8
kheraba1221 <122232018+kheraba1221@users.noreply.github.com>
2023-02-01 19:33:28 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,54 @@
+i = 0
+bclicked = 0
+atimes = 0
+def interact(interval: number):
+    global i
+    basic.clear_screen()
+    i = 1
+    while i < interval + 1:
+        basic.show_string("" + str(expo(i)))
+        basic.pause(100)
+        basic.clear_screen()
+        i += 1
+def expo(n: number):
+    
+        return 3*(n*n)+n+1
+def numElements():
+    global atimes, bclicked
+    while bclicked < 1:
+        if input.button_is_pressed(Button.A):
+            basic.show_string("A")
+            basic.clear_screen()
+            atimes += 1
+            basic.show_string("" + str(atimes))
+        elif input.button_is_pressed(Button.B):
+            basic.show_string("B")
+            bclicked += 1
+        basic.pause(50)
+    basic.clear_screen()
+    basic.show_string("Expo")
+    interact(atimes)
+def showIcon():
+    basic.clear_screen()
+    music.start_melody(music.built_in_melody(Melodies.ENTERTAINER),
+        MelodyOptions.ONCE)
+    for index in range(4):
+        basic.show_icon(IconNames.SMALL_DIAMOND)
+        basic.pause(100)
+        basic.show_icon(IconNames.DIAMOND)
+        basic.pause(100)
+    basic.clear_screen()
+    basic.show_string("Expo")
+    basic.clear_screen()
+
+def on_forever():
+    global atimes, bclicked
+    music.set_built_in_speaker_enabled(True)
+    atimes = 0
+    bclicked = 0
+    showIcon()
+    basic.show_string("A to input B to Expo")
+    numElements()
+    basic.pause(500)
+basic.forever(on_forever)
+
---
+++ main.ts
@@ -0,0 +1,63 @@
+let i = 0
+let bclicked = 0
+let atimes = 0
+function interact(interval: number) {
+    
+    basic.clearScreen()
+    i = 1
+    while (i < interval + 1) {
+        basic.showString("" + ("" + expo(i)))
+        basic.pause(100)
+        basic.clearScreen()
+        i += 1
+    }
+}
+
+function expo(n: number) {
+    return 3 * (n * n) + n + 1
+}
+
+function numElements() {
+    
+    while (bclicked < 1) {
+        if (input.buttonIsPressed(Button.A)) {
+            basic.showString("A")
+            basic.clearScreen()
+            atimes += 1
+            basic.showString("" + ("" + atimes))
+        } else if (input.buttonIsPressed(Button.B)) {
+            basic.showString("B")
+            bclicked += 1
+        }
+        
+        basic.pause(50)
+    }
+    basic.clearScreen()
+    basic.showString("Expo")
+    interact(atimes)
+}
+
+function showIcon() {
+    basic.clearScreen()
+    music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.SmallDiamond)
+        basic.pause(100)
+        basic.showIcon(IconNames.Diamond)
+        basic.pause(100)
+    }
+    basic.clearScreen()
+    basic.showString("Expo")
+    basic.clearScreen()
+}
+
+basic.forever(function on_forever() {
+    
+    music.setBuiltInSpeakerEnabled(true)
+    atimes = 0
+    bclicked = 0
+    showIcon()
+    basic.showString("A to input B to Expo")
+    numElements()
+    basic.pause(500)
+})
